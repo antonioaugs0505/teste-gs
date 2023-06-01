011abf076d39de37be97d45c50d6d188d70ba005
antonioaugs0505 <rm550344@fiap.com.br>
2023-06-01 20:07:54 -0300

    teste1

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,10 @@
     let menuMobile = document.querySelector('.mobile-menu');
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/cardapio.png"
+        document.querySelector('.icon').src = "../img/menu-aberto.png"
     } else{
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "img/fechado.png"
+        document.querySelector('.icon').src = "../img/menu-fechado.png"
     }
  })
 
